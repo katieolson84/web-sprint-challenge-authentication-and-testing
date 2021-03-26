6f20952eb12c18e47813ce11b3e28071a976d7b2
katieolson84 <katieolson84@gmail.com>
2021-03-26 09:30:47 -0600

    server test is passing

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,19 @@
 // Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
+const server = require('./server');
+const request = require('supertest');
+const db = require('../data/dbConfig');
+
+beforeAll(async () => {
+  await db.migrate.rollback()
+  await db.migrate.latest()
+})
+beforeEach(async () => {
+  await db('users').truncate()
+})
+afterAll(async () => {
+  await db.destroy()
+})
+
+it('process.env.NODE_ENV must be "testing"', () => {
+  expect(process.env.NODE_ENV).toBe('testing')
 })
