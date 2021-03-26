df1b53063b1eaf9db4e3077cbc427b1791abb75f
katieolson84 <katieolson84@gmail.com>
2021-03-26 12:04:57 -0600

    test for find by user id works

--- api/server.test.js
+++ api/server.test.js
@@ -3,6 +3,7 @@ const server = require('./server');
 const request = require('supertest');
 const db = require('../data/dbConfig');
 const bcrypt = require('bcryptjs');
+const User = require('./users/users-model')
 
 beforeAll(async () => {
   await db.migrate.rollback()
@@ -44,5 +45,10 @@ describe('users endpoints', () => {
       .send({ username: 'katie', password: '1234abcd' })
       expect(res.body.message).toMatch('invalid credentials')
     }, 500)
+    it('can find a user by the id ', async () => {
+      await db('users').insert({username: "katie", password: "1234"})
+      const katie= await User.getById(1)
+      expect(katie).toMatchObject({ id: 1, username: "katie"})
+    })
   })
 })
--- data/seeds/01-users.js
+++
@@ -1,7 +0,0 @@
-
-exports.seed = function (knex) {
-  return knex('users').insert({
-    username: 'katie',
-    password: "1234"
-  })
-};
